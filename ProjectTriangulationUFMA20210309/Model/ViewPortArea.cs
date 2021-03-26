34066de397ea3d27e3617c07963275616a4c481f
j3diniz <j3di@uol.com.br>
2021-03-26 17:12:11 -0300

    Add project files.

---
+++ ProjectTriangulationUFMA20210309/Model/ViewPortArea.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectTriangulationUFMA20210309.Model {
+    class ViewPortArea {
+
+        #region Fields, Properties and Variables
+        private double xMin;
+        public double XMin {
+            get { return xMin; }
+            set { xMin = value; }
+        }
+
+        private double xMax;
+        public double XMax {
+            get { return xMax; }
+            set { xMax = value; }
+        }
+
+        private double yMin;
+        public double YMin {
+            get { return yMin; }
+            set { yMin = value; }
+        }
+
+        private double yMax;
+        public double YMax {
+            get { return yMax; }
+            set { yMax = value; }
+        }
+        #endregion
+
+        #region Constructors
+        public ViewPortArea() {
+        }
+
+        public ViewPortArea(double xMin, double xMax, double yMin, double yMax) {
+            this.xMin = xMin;
+            this.xMax = xMax;
+            this.yMin = yMin;
+            this.yMax = yMax;
+        }
+        #endregion
+
+        #region Normalize
+        public double XNormalize(double x, double viewPortWidth) {
+            double result = (x - xMin) * viewPortWidth / (xMax - xMin);
+            return result;
+        }
+
+        public double YNormalize(double y, double viewPortHeight) {
+            double result = viewPortHeight - (y - yMin) * viewPortHeight / (yMax - yMin);
+            return result;
+        }
+        #endregion
+
+        // ToDo: Implement ToString
+
+    }
+}
---
+++ ProjectTriangulationUFMA20210309/View/MainView.xaml.cs
@@ -0,0 +1,57 @@
+﻿using ProjectTriangulationUFMA20210309.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProjectTriangulationUFMA20210309 {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+
+        private readonly ViewPortArea viewPortArea;
+        private Line path;
+
+        public MainWindow() {
+            InitializeComponent();
+            viewPortArea= new ViewPortArea(0, 1000, 0, 1000);
+
+            UpdateGraphics();
+        }
+
+        #region Events
+        private void Exit_OnClick(object sender, RoutedEventArgs e) {
+            this.Close();
+        }
+        private void UpdateGraphics_OnClick(object sender, RoutedEventArgs e) {
+            UpdateGraphics();
+        }
+        #endregion
+
+        private void UpdateGraphics() {
+            viewPortCanvas.Children.Remove(path);
+            path = new Line();
+            path.X1 = viewPortArea.XNormalize(Convert.ToDouble(txtTXstart.Text), viewPortCanvas.Width);
+            path.Y1 = viewPortArea.YNormalize(Convert.ToDouble(txtTYstart.Text), viewPortCanvas.Height);
+            path.X2 = viewPortArea.XNormalize(Convert.ToDouble(txtTXend.Text), viewPortCanvas.Width);
+            path.Y2 = viewPortArea.YNormalize(Convert.ToDouble(txtTYend.Text), viewPortCanvas.Height);
+            path.Stroke = Brushes.Blue;
+            path.StrokeThickness = 2;
+            viewPortCanvas.Children.Add(path);
+
+
+        }
+
+    }
+}
