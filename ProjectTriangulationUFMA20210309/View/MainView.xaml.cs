eb5e1080eaee7fc411d21c27f91627cfc5bb0072
j3diniz <j3di@uol.com.br>
2021-04-19 22:13:04 -0300

    Loading Points

--- ProjectTriangulationUFMA20210309/View/MainView.xaml.cs
+++ ProjectTriangulationUFMA20210309/View/MainView.xaml.cs
@@ -1,6 +1,8 @@
-﻿using ProjectTriangulationUFMA20210309.Model;
+﻿using Microsoft.Win32;
+using ProjectTriangulationUFMA20210309.Model;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,32 +24,13 @@ namespace ProjectTriangulationUFMA20210309 {
     public partial class MainWindow : Window {
 
         private readonly ViewPortArea viewPortArea;
-        private Line pathT;
-        private Line pathV;
-        private Ellipse circleAtPoint;
-        private IntersectionPoint intersectionPoint;
-
-        // Line T
-        private LineSegment lineT;
-        private PointXY pointStartT;
-        private PointXY pointEndT;
-        private double tXstart;
-        private double tYstart;
-        private double tXend;
-        private double tYend;
-
-        // Line V
-        private LineSegment lineV;
-        private PointXY pointStartV;
-        private PointXY pointEndV;
-        private double vXstart;
-        private double vYstart;
-        private double vXend;
-        private double vYend;
+        private List<PointXY> points;
 
         public MainWindow() {
             InitializeComponent();
-            viewPortArea= new ViewPortArea(0, 1000, 0, 1000);
+            viewPortArea = new ViewPortArea(0, 1000, 0, 1000);
+            // points = new List<PointXY> { new PointXY(50, 100), new PointXY(500, 500), new PointXY(50, 700), new PointXY(800, 800), new PointXY(800, 50) };
+            points = new List<PointXY>();
 
             UpdateGraphics();
         }
@@ -63,76 +46,48 @@ namespace ProjectTriangulationUFMA20210309 {
 
         private void UpdateGraphics() {
 
-            // Line T
-            tXstart = Convert.ToDouble(txtTXstart.Text);
-            tYstart = Convert.ToDouble(txtTYstart.Text);
-            tXend = Convert.ToDouble(txtTXend.Text);
-            tYend = Convert.ToDouble(txtTYend.Text);
+            viewPortCanvas.Children.Clear();
 
-            // Line V
-            vXstart = Convert.ToDouble(txtVXstart.Text);
-            vYstart = Convert.ToDouble(txtVYstart.Text);
-            vXend = Convert.ToDouble(txtVXend.Text);
-            vYend = Convert.ToDouble(txtVYend.Text);
+            SolidColorBrush solidColorBrush = new SolidColorBrush(Color.FromRgb(50, 50, 50));
 
-            viewPortCanvas.Children.Remove(pathT);
-            pathT = new Line();
-            pathT.X1 = viewPortArea.XNormalize(tXstart, viewPortCanvas.Width);
-            pathT.Y1 = viewPortArea.YNormalize(tYstart, viewPortCanvas.Height);
-            pathT.X2 = viewPortArea.XNormalize(tXend, viewPortCanvas.Width);
-            pathT.Y2 = viewPortArea.YNormalize(tYend, viewPortCanvas.Height);
-            pathT.Stroke = Brushes.Blue;
-            pathT.StrokeThickness = 2;
-            viewPortCanvas.Children.Add(pathT);
-
-            viewPortCanvas.Children.Remove(pathV);
-            pathV = new Line();
-            pathV.X1 = viewPortArea.XNormalize(vXstart, viewPortCanvas.Width);
-            pathV.Y1 = viewPortArea.YNormalize(vYstart, viewPortCanvas.Height);
-            pathV.X2 = viewPortArea.XNormalize(vXend, viewPortCanvas.Width);
-            pathV.Y2 = viewPortArea.YNormalize(vYend, viewPortCanvas.Height);
-            pathV.Stroke = Brushes.Red;
-            pathV.StrokeThickness = 2;
-            viewPortCanvas.Children.Add(pathV);
-
-            // Line T
-            PointXY pointStartT = new PointXY(tXstart, tYstart);
-            PointXY pointEndT = new PointXY(tXend, tYend);
-            LineSegment lineT = new LineSegment(pointStartT, pointEndT);
-
-            // Line V
-            PointXY pointStartV = new PointXY(vXstart, vYstart);
-            PointXY pointEndV = new PointXY(vXend, vYend);
-            LineSegment lineV = new LineSegment(pointStartV, pointEndV);
-
-            // Intersection Point
-            intersectionPoint = new IntersectionPoint(lineT, lineV);
-            if (intersectionPoint.Intersection) {
-                //lblIntersection.Text = "Intersection? true!";
-            } else {
-                //lblIntersection.Text = "Intersection? false!";
+            if ((points != null) && (points.Count > 0)) {
+                foreach (PointXY point in points) {
+                    Ellipse circleAtPoint = new Ellipse();
+                    circleAtPoint.Fill = solidColorBrush;
+                    circleAtPoint.Stroke = Brushes.Red;
+                    circleAtPoint.StrokeThickness = 2;
+                    circleAtPoint.Width = 10;
+                    circleAtPoint.Height = 10;
+                    Canvas.SetLeft(circleAtPoint, (viewPortArea.XNormalize(point.X, viewPortCanvas.Width) - 5));
+                    Canvas.SetTop(circleAtPoint, (viewPortArea.YNormalize(point.Y, viewPortCanvas.Height) - 5));
+                    viewPortCanvas.Children.Add(circleAtPoint);
+                }
             }
-            txtPointX.Text = intersectionPoint.PointOfIntersection.X.ToString();
-            txtPointY.Text = intersectionPoint.PointOfIntersection.Y.ToString();
-
-            txtTb.Text = intersectionPoint.LineT.B.ToString();
-            txtTm.Text = intersectionPoint.LineT.M.ToString();
-
-            txtVb.Text = intersectionPoint.LineV.B.ToString();
-            txtVm.Text = intersectionPoint.LineV.M.ToString();
-
-            viewPortCanvas.Children.Remove(circleAtPoint);
-            circleAtPoint = new Ellipse();
-            circleAtPoint.Fill = new SolidColorBrush(Color.FromRgb(50, 50, 50));
-            circleAtPoint.Stroke = Brushes.Red;
-            circleAtPoint.StrokeThickness = 2;
-            circleAtPoint.Width = 10;
-            circleAtPoint.Height = 10;
-            Canvas.SetLeft(circleAtPoint, (viewPortArea.XNormalize(intersectionPoint.PointOfIntersection.X, viewPortCanvas.Width) - 5));
-            Canvas.SetTop(circleAtPoint, (viewPortArea.YNormalize(intersectionPoint.PointOfIntersection.Y, viewPortCanvas.Height) - 5));
-            viewPortCanvas.Children.Add(circleAtPoint);
 
         }
 
+        private void LoadPoints_OnClick(object sender, RoutedEventArgs e) {
+            try {
+                string arquivo = "";
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                openFileDialog.Title = "Abrir Cotas";
+                openFileDialog.InitialDirectory = @"C:\Temp\UFMA";
+                openFileDialog.Filter = "Rtf documents|*.rtf|Txt files (*.txt)|*.txt|Csv files (*.csv)|*.csv|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+                if (openFileDialog.ShowDialog() == true) {
+                    arquivo = openFileDialog.FileName;
+                }
+
+                string[] arquivoLinhas = File.ReadAllLines(arquivo);
+                for (int i = 0; i < arquivoLinhas.Length; i++) {
+                    string[] auxiliar = arquivoLinhas[i].Split('/', ';', ',');
+                    points.Add(new PointXY(Convert.ToDouble(auxiliar[0]), Convert.ToDouble(auxiliar[1])));
+                }
+                //anagramas.Dicionario = dicionario;
+            } catch (Exception ex) {
+                MessageBox.Show(ex.Message, "Error!");
+            }
+        }
     }
 }
